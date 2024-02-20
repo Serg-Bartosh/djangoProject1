2be106c02de9f9eea2ef7315f27f631e006178c6
onehayzent <sergbartosh@gmail.com>
2024-02-20 19:38:00 +0200

    test

--- socialnetwork/serializers/UserSerializer.py
+++ socialnetwork/serializers/UserSerializer.py
@@ -5,5 +5,5 @@ from socialnetwork.model.abstract_user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_active',
-                  'is_staff', 'is_superuser']
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
